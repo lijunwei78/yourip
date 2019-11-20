b8a11a056161e04c5b3309cd435047601b088ee5
Jian Yang <yangj@njupt.edu.cn>
2019-11-20 22:43:59 +0800

    114.221.19.105

--- updateip.sh
+++ updateip.sh
@@ -5,6 +5,7 @@ oldip=$(cat yourip)
 newip=$(curl ip.6655.com/ip.aspx)
 if [[ "$oldip" != "$newip" ]]; then
 	echo $newip > yourip
+	echo "IP change to $newip" >log
 	git commit -a -m "$newip" && git push origin master
 fi
 
