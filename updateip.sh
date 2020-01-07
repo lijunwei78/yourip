3d33b9188904a25d1d84026168b312757b0e53dc
Jian Yang <yangj@njupt.edu.cn>
2020-01-07 17:05:45 +0800

    114.221.22.148

--- updateip.sh
+++ updateip.sh
@@ -2,10 +2,20 @@
 
 git pull
 oldip=$(cat yourip)
-newip=$(curl inet-ip.info)
+newip1=$(curl inet-ip.info)
+newip2=$(dig +short myip.opendns.com @resolver1.opendns.com)
+if [[ "$newip1" == "$newip2" ]];then
+	newip=$newip1
+else
+	newip=$(curl whatismyip.akamai.com)
+fi
+lasttime=$(date)
+echo "Last check time:$(date)" > log
+#newip3=$(curl whatismyip.akamai.com)
+
 if [[ "$oldip" != "$newip" ]]; then
 	echo $newip > yourip
-	echo "IP change to $newip" >log
+	echo "IP change to $newip" >>log
 	git commit -a -m "$newip" && git push origin master
 fi
 
